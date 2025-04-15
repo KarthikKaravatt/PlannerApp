494d4085809be9da8205b6e335dfdeadabcb467a
kk <karthikkaravatt@gmail.com>
2025-04-15 19:05:12 +0800

    format and ignore dist file for biome

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,14 @@ import { createFileRoute } from "@tanstack/react-router";
 import logo from "../logo.svg";
 
 export const Route = createFileRoute("/")({
-  component: App,
+	component: App,
 });
 
 function App() {
-  return (
-    <div className="text-center">
-      <header
-        className="
+	return (
+		<div className="text-center">
+			<header
+				className="
         bg-[#282c34] 
         min-h-dvh 
         flex 
@@ -19,32 +19,32 @@ function App() {
         text-xl 
         text-white
         "
-      >
-        <img
-          src={logo}
-          className="h-40 pointer-events-none animate-spin"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-sky-500"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-sky-500"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
-  );
+			>
+				<img
+					src={logo}
+					className="h-40 pointer-events-none animate-spin"
+					alt="logo"
+				/>
+				<p>
+					Edit <code>src/routes/index.tsx</code> and save to reload.
+				</p>
+				<a
+					className="text-sky-500"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<a
+					className="text-sky-500"
+					href="https://tanstack.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn TanStack
+				</a>
+			</header>
+		</div>
+	);
 }
