ea446174b1cbb23f72832329ac9692fa90f5f27f
kk <karthikkaravatt@gmail.com>
2025-04-18 16:10:15 +0800

    add tasks

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -12,12 +12,19 @@
 
 import { Route as rootRoute } from './routes/__root'
 import { Route as IndexImport } from './routes/index'
+import { Route as PlannerIndexImport } from './routes/planner/index'
 
 // Create/Update Routes
 
 const IndexRoute = IndexImport.update({
   id: '/',
   path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const PlannerIndexRoute = PlannerIndexImport.update({
+  id: '/planner/',
+  path: '/planner/',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -32,6 +39,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/planner/': {
+      id: '/planner/'
+      path: '/planner'
+      fullPath: '/planner'
+      preLoaderRoute: typeof PlannerIndexImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -39,32 +53,37 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/planner': typeof PlannerIndexRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/planner': typeof PlannerIndexRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/planner/': typeof PlannerIndexRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/planner'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/planner'
+  id: '__root__' | '/' | '/planner/'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  PlannerIndexRoute: typeof PlannerIndexRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  PlannerIndexRoute: PlannerIndexRoute,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +96,15 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/planner/"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/planner/": {
+      "filePath": "planner/index.tsx"
     }
   }
 }
---
+++ src/routes/planner/index.tsx
@@ -0,0 +1,88 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+import { useState, type ChangeEvent } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+
+export const Route = createFileRoute("/planner/")({
+  component: RouteComponent,
+});
+
+interface TaskProp {
+  label: string;
+  completed: boolean;
+  id: string;
+}
+
+function RouteComponent() {
+  const initialTasksData: [string, TaskProp][] = [
+    { label: "Task 1", completed: false },
+    { label: "Task 2", completed: false },
+    { label: "Task 3", completed: false },
+  ].map((taskBase) => {
+    const id = uuidv4();
+    return [id, { ...taskBase, id }];
+  });
+  const initialTasks: Map<string, TaskProp> = new Map(initialTasksData);
+  const [tasks, setTasks] = useState<Map<string, TaskProp>>(initialTasks);
+  const [inputTask, setInputTask] = useState<string>("");
+  const onInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputTask(event.target.value);
+  };
+  const onAddButtonClick = () => {
+    setTasks((prev) => {
+      const newTaskMap = new Map(prev);
+      if (inputTask.trim().length !== 0) {
+        const id = uuidv4();
+        newTaskMap.set(id, {
+          label: inputTask,
+          completed: false,
+          id: id,
+        });
+      }
+      return newTaskMap;
+    });
+  };
+  const onCheckedChange = (
+    key: string,
+    event: ChangeEvent<HTMLInputElement>,
+  ) => {
+    const isChecked = event.target.checked;
+    setTasks((prev) => {
+      const newTaskMap = new Map(prev);
+      const task = prev.get(key);
+      if (task !== undefined) {
+        const updatedTask: TaskProp = {
+          ...task,
+          completed: isChecked,
+        };
+        newTaskMap.set(key, updatedTask);
+      }
+      return newTaskMap;
+    });
+  };
+  return (
+    <div>
+      <ul>
+        {Array.from(tasks).map(([_, item]) => (
+          <li key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={(event) => onCheckedChange(item.id, event)}
+            />
+            {item.label}
+          </li>
+        ))}
+      </ul>
+      <button className="border-1" type="button" onClick={onAddButtonClick}>
+        Add task
+      </button>
+      <input
+        className="border-1 border-l-0"
+        placeholder="Task name"
+        onChange={onInputChanged}
+      />
+    </div>
+  );
+}
