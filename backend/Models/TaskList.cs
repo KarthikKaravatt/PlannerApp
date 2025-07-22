dc6125bd068dac9708cad63325ea8a36f45289df
kk <karthikkaravatt@gmail.com>
2025-07-22 11:33:59 +0800

    add max length to task list name

--- backend/Models/TaskList.cs
+++ backend/Models/TaskList.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography.X509Certificates;
 
 namespace backend.Models;
@@ -5,9 +6,11 @@ namespace backend.Models;
 public class TaskList
 {
     public required Guid Id { get; set; }
+    
+    [StringLength(256)]
     public required string Name { get; set; }
     public required uint OrderIndex { get; set; }
-    public ICollection<Task> Tasks { get; set; } = new List<Task>();
+    public ICollection<Task> Tasks { get; init; } = new List<Task>();
 
     public TaskList(string name, ICollection<Task> tasks, uint orderIndex, Guid id)
     {
