88e212a08a955bb7393db0ab3c3a8d0bea8bf22e
kk <karthikkaravatt@gmail.com>
2025-07-25 11:28:25 +0800

    add optoin to use tool tip in popover

--- frontend/src/app/General/PopOverMenu.tsx
+++ frontend/src/app/General/PopOverMenu.tsx
@@ -1,23 +1,28 @@
 import type React from "react";
 import { Button, Dialog, DialogTrigger, Popover } from "react-aria-components";
 import { BsThreeDots } from "react-icons/bs";
+import { Tooltip } from "./ToolTip.tsx";
 
 interface PopOverMenuProps {
   children: React.ReactNode;
+  hoverMessage?: string;
   isDisabled?: boolean;
 }
 
 export const PopOverMenu: React.FC<PopOverMenuProps> = ({
+  hoverMessage,
   children,
   isDisabled,
 }) => {
   return (
     <DialogTrigger>
-      <Button isDisabled={isDisabled ?? false}>
-        <BsThreeDots />
-      </Button>
+      <Tooltip message={hoverMessage ?? ""}>
+        <Button isDisabled={isDisabled ?? false}>
+          <BsThreeDots />
+        </Button>
+      </Tooltip>
       <Popover>
-        <Dialog className="border-1 border-gray-300 dark:border-white bg-sky-100 dark:bg-dark-background-c text-sm gap-y-0.5 flex flex-col p-0.5 text-blue-950 dark:text-white rounded-md">
+        <Dialog className="flex flex-col gap-y-0.5 rounded-md border-1 border-gray-300 bg-sky-100 p-0.5 text-sm text-blue-950 dark:border-white dark:bg-dark-background-c dark:text-white">
           {children}
         </Dialog>
       </Popover>
