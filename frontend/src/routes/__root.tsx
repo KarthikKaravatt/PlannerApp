0b32ca6c6cc59f92842d36759cc2f414137e6596
kk <karthikkaravatt@gmail.com>
2025-07-26 12:03:54 +0800

    dont use them swticher header right now

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,21 +1,28 @@
-// import { ThemeSwitcher } from "@/app/ThemeSwitcher";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+// import { lazy, Suspense } from "react";
+// import { FaSpinner } from "react-icons/fa6";
+
+// const ThemeSwitcher = lazy(() =>
+//   import("@/app/ThemeSwitcher").then((module) => ({
+//     default: module.ThemeSwitcher,
+//   })),
+// );
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      {/* <header */}
-      {/* 	className=" */}
-      {/*      flex justify-end */}
-      {/*      bg-blue-100 dark:bg-dark-background-c */}
-      {/*      backdrop-blur-sm */}
-      {/*      " */}
-      {/* > */}
-      {/* 	<ThemeSwitcher /> */}
+      {/* <header className=" flex justify-end bg-blue-100 backdrop-blur-sm dark:bg-dark-background-c "> */}
+      {/*   <Suspense */}
+      {/*     fallback={ */}
+      {/*       <div className="p-2"> */}
+      {/*         <FaSpinner className="animate-spin" /> */}
+      {/*       </div> */}
+      {/*     } */}
+      {/*   > */}
+      {/*     <ThemeSwitcher /> */}
+      {/*   </Suspense> */}
       {/* </header> */}
       <Outlet />
-      {/* <TanStackRouterDevtools /> */}
     </>
   ),
 });
