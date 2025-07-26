d5e17800c771c759afe563e92c1e2c7c0268e593
kk <karthikkaravatt@gmail.com>
2025-07-26 11:29:13 +0800

    allow prod server in cros

--- backend/Program.cs
+++ backend/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddCors((options =>
     options.AddDefaultPolicy(policyBuilder =>
     {
         // For development without Cors errors
+        policyBuilder.WithOrigins("http://localhost:4173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
         policyBuilder.WithOrigins("http://localhost:3000")
             .AllowAnyHeader()
             .AllowAnyMethod();
