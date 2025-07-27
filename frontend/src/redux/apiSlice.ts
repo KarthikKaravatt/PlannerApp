a369b2940be5942dd414f3f2b540f90a19d78c4c
kk <karthikkaravatt@gmail.com>
2025-07-27 23:12:33 +0800

    remvoe reducer path

--- frontend/src/redux/apiSlice.ts
+++ frontend/src/redux/apiSlice.ts
@@ -3,7 +3,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiUrl = `${import.meta.env.VITE_BACKEND_APP_API_URL}/taskLists`;
 
 export const apiSlice = createApi({
-  reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
   tagTypes: [
     "IncompleteTasks",
