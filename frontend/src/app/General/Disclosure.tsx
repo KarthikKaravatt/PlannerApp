6018bb905169ebe136c8f0ffd41c748bd4c879a2
kk <karthikkaravatt@gmail.com>
2025-07-30 18:31:24 +0800

    use a grid to make closing and openign animations smooth

--- frontend/src/app/General/Disclosure.tsx
+++ frontend/src/app/General/Disclosure.tsx
@@ -18,20 +18,24 @@ export const CustomDisclosure = ({
   ...props
 }: CustomDisclosureProps) => {
   return (
-    <Disclosure {...props} className={"group"}>
+    <Disclosure {...props} className="group">
       <Heading>
-        <Button slot="trigger">
+        <Button slot="trigger" className="w-full text-left">
           <div className="flex flex-row items-center gap-1 p-1">
-            <div className="transition-transform duration-200 ease-in group-data-[expanded]:rotate-90">
+            <div className="transition-transform duration-300 ease-in-out group-data-[expanded]:rotate-90">
               <MdOutlineArrowForwardIos />
             </div>
             {title}
           </div>
         </Button>
       </Heading>
-      <DisclosurePanel className=" -translate-y-2 transform opacity-0 transition-all duration-100 ease-in-out group-data-[expanded]:translate-y-0 group-data-[expanded]:opacity-100 ">
-        {children}
-      </DisclosurePanel>
+      <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-300 ease-in-out group-data-[expanded]:grid-rows-[1fr]">
+        <div className="min-h-0 overflow-hidden">
+          <DisclosurePanel className="-translate-y-1 p-1 opacity-0 transition-all duration-200 ease-out group-data-[expanded]:translate-y-0 group-data-[expanded]:opacity-100 group-data-[expanded]:delay-100">
+            {children}
+          </DisclosurePanel>
+        </div>
+      </div>
     </Disclosure>
   );
 };
