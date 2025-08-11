3d048d1533e216c08c3bacf2472143f23cc7325c
kk <karthikkaravatt@gmail.com>
2025-08-11 11:02:10 +0800

    fix tag schema

--- frontend/src/schemas/tag.ts
+++ frontend/src/schemas/tag.ts
@@ -3,16 +3,17 @@ import { z } from "zod";
 const MAX_LIGHTNESSS = 100;
 
 export const colourSchema = z.object({
-  l: z.float32().min(0).max(MAX_LIGHTNESSS),
-  c: z.float32().min(0),
-  h: z.float32().min(0),
+  l: z.number().min(0).max(MAX_LIGHTNESSS),
+  c: z.number().min(0),
+  h: z.number().min(0),
 });
 
-export const tagSchmea = z.object({
+export const tagSchema = z.object({
   id: z.uuidv7(),
   name: z.string(),
-  color: colourSchema,
+  colour: colourSchema,
 });
-export type Tag = z.infer<typeof tagSchmea>;
+
+export type Tag = z.infer<typeof tagSchema>;
 
 export type Colour = z.infer<typeof colourSchema>;
